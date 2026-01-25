46a61a0373a4ef4aed68fdf8d81f788ec0cb5cdb
nghiaho310pf <164844348+nghiaho310pf@users.noreply.github.com>
2026-01-25 12:08:53 +0700

    Initial MVVM architecture

---
+++ lib/models/money_model.dart
@@ -0,0 +1,5 @@
+class MoneyModel {
+  int totalSpent;
+
+  MoneyModel({this.totalSpent = 0});
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,19 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:adv_money_mana/main.dart';
+// Replace 'your_app_package' with your actual package name defined in pubspec.yaml
+import 'package:adv_money_mana/main.dart'; 
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+  testWidgets('Money View updates smoke test', (WidgetTester tester) async {
     await tester.pumpWidget(const MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.text('\$0'), findsOneWidget);
+    expect(find.text('\$10'), findsNothing);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    await tester.tap(find.text('Spend \$10'));
+
     await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('\$0'), findsNothing);
+    expect(find.text('\$10'), findsOneWidget);
   });
 }
