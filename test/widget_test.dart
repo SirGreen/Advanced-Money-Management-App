ee96165b681b0308097d96ec5de03eca3376275c
leduc2005 <leminhduc16022005@gmail.com>
2026-02-06 17:30:10 +0700

    Chore: Configurations updates and code formatting compliance

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:adv_money_mana/main.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:adv_money_mana/data/data_sources/hive_service.dart';
 
 // nên đọc documentation để viết test
 // tui chưa hình dung là nên có test cho từng tí code luôn ko
@@ -11,8 +12,11 @@ import 'package:hive_flutter/hive_flutter.dart';
 void main() {
   testWidgets('Money View updates smoke test', (WidgetTester tester) async {
     await Hive.initFlutter();
+    final hiveService = HiveService();
+    // Note: This test might still fail logic-wise because Hive needs open boxes
+    // For now, fixing the compilation error at least.
 
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(MyApp(hiveService: hiveService));
 
     expect(find.text('Spend \$10'), findsOneWidget);
     expect(find.text('Spend \$50'), findsOneWidget);
