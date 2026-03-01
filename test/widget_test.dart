2b0676f2283c2a9e961751e781a2717b09d5b5ce
nghiaho310pf <164844348+nghiaho310pf@users.noreply.github.com>
2026-03-01 11:16:39 +0700

    Mock just enough things to have a passing test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:mocktail/mocktail.dart';
 
 import 'package:adv_money_mana/main.dart';
 
@@ -11,14 +12,13 @@ import 'package:adv_money_mana/domain/entities/tag.dart';
 import 'package:adv_money_mana/domain/entities/scheduled_expenditure.dart';
 import 'package:adv_money_mana/domain/entities/settings.dart';
 
-import 'package:adv_money_mana/data/data_sources/settings_service.dart';
 import 'package:adv_money_mana/data/data_sources/tag_local_data_source.dart';
 import 'package:adv_money_mana/data/data_sources/expenditure_service.dart';
 import 'package:adv_money_mana/data/data_sources/scheduled_expenditure_service.dart';
 import 'package:adv_money_mana/data/data_sources/llm_service.dart';
 import 'package:adv_money_mana/data/services/notification_service.dart';
 
-import 'package:adv_money_mana/data/repositories/settings_repository_impl.dart';
+import 'package:adv_money_mana/domain/repositories/settings_repository.dart';
 import 'package:adv_money_mana/data/repositories/tag_repository_impl.dart';
 import 'package:adv_money_mana/data/repositories/expenditure_repository_impl.dart';
 import 'package:adv_money_mana/data/repositories/scheduled_expenditure_repository_impl.dart';
@@ -28,6 +28,16 @@ import 'package:adv_money_mana/domain/usecases/scan_receipt_usecase.dart';
 import 'package:adv_money_mana/domain/services/recurring_transaction_service.dart';
 
 import 'package:adv_money_mana/ui/settings/settings_view_model.dart';
+
+class MockNotificationService extends Mock implements NotificationService {}
+
+class FakeSettingsRepository implements SettingsRepository {
+  @override
+  Future<Settings> getSettings() async => Settings();
+
+  @override
+  Future<void> saveSettings(Settings settings) async {}
+}
 
 void main() {
   late Directory tempDir;
@@ -53,10 +63,12 @@ void main() {
     Hive.registerAdapter(SettingsAdapter());
 
     await initializeDateFormatting('vi_VN', null);
-    final notificationService = NotificationService();
-    await notificationService.init();
 
-    final settingsRepository = SettingsRepositoryImpl(SettingsService());
+    final mockNotificationService = MockNotificationService();
+    when(() => mockNotificationService.init()).thenAnswer((_) async {});
+    await mockNotificationService.init();
+
+    final settingsRepository = FakeSettingsRepository();
     final tagRepository = TagRepositoryImpl(TagLocalDataSource());
     final expenditureRepository = ExpenditureRepositoryImpl(
       ExpenditureService(),
@@ -71,7 +83,7 @@ void main() {
     final recurringService = RecurringTransactionService(
       scheduledRepository,
       expenditureRepository,
-      notificationService,
+      mockNotificationService,
     );
 
     final settingsViewModel = SettingsViewModel(repository: settingsRepository);
